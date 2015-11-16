c0c5ec151a1d7248fb47b95b03725a70367bce71
Mark Aves <markaves@gmail.com>
2015-11-16 17:40:26 +0800

    New Commit

--- src/biztalkmonitoring/BiztalkMonitoring.java
+++ src/biztalkmonitoring/BiztalkMonitoring.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package biztalkmonitoring;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -14,8 +18,19 @@ public class BiztalkMonitoring {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+         Runtime runtime = Runtime.getRuntime();
+        Process proc = runtime.exec("powershell.exe C:\\Temp\\biztalkquery.ps1");
+        InputStream is = proc.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader reader = new BufferedReader(isr);
+        String line;
+        while ((line = reader.readLine()) != null)
+        {
+            System.out.println(line);
+        }
+        reader.close();
+        proc.getOutputStream().close();
     }
     
 }
